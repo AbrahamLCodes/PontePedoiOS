f7964fcb8a50a576a71c34a6aa6c67a19410f275
Abraham Luna <abraham11999@hotmail.com>
2020-12-23 12:58:50 -0700

    Main Screen responsive para Portrait Orientation

--- Ponte Pedo/ViewController.swift
+++ Ponte Pedo/ViewController.swift
@@ -9,6 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var imagen: UIImageView!
+    @IBOutlet weak var como: UILabel!
+    @IBOutlet weak var ncarta: UILabel!
+    @IBOutlet weak var efecto: UILabel!
+    
+    let fontNCarta : CGFloat =  0.060
+    let fontEfecto : CGFloat = 0.070
+    let fontComo : CGFloat = 0.05
+    
+    
+    //Method to prepare a Label to ve shown as an Android Toast
+    func showToast(message : String, font: UIFont) {
+
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = font
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 10.0, delay: 0.1, options: .curveEaseOut, animations: {
+             toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+    
+    //Component to prepare an ImageBiew as a Background
     var imageView: UIImageView = {
             let imageView = UIImageView(frame: .zero)
             imageView.image = UIImage(named: "fondoobscuro.png")
@@ -17,11 +49,8 @@ class ViewController: UIViewController {
             return imageView
         }()
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+    //Method to set an Image as a Background in the App
+    func setBackgroundImage(){
         view.insertSubview(imageView, at: 0)
                 NSLayoutConstraint.activate([
                     imageView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -30,5 +59,24 @@ class ViewController: UIViewController {
                     imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
                 ])
     }
+    
+    //Method to resize label font size by screen size
+    func initLabelsFont(){
+        ncarta.font = ncarta.font.withSize(self.view.frame.width * fontNCarta)
+        efecto.font = efecto.font.withSize(self.view.frame.width * fontEfecto)
+        como.font = como.font.withSize(self.view.frame.width * fontComo)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setBackgroundImage()
+        initLabelsFont()
+    }
 }
 
+/*
+Make a Toast
+ self.showToast(message: "Este es un Toast de Inicio", font: .systemFont(ofSize: 12.0))
+ 
+ */
